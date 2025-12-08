cd7c4c17c6e9e7163a677d38269af65cb8f7bec8
Ja4yJa5y <jalghamdi306@gmail.com>
2025-12-08 19:37:48 +0300

    Add files via upload

--- LibraryItems.java
+++ LibraryItems.java
@@ -11,32 +11,33 @@ import java.util.Scanner;
  *
  * @author Ja454
  */
+import java.util.ArrayList;
+
 public class LibraryItems {
-      private int Id;
-    private String  Title ;
-    private String Author ;
+    private int Id;
+    private String Title;
+    private String Author;
     private double Price;
     private String Genre;
     private String type;
-    public static ArrayList<LibraryItems> libraryItemsList = new ArrayList<>();
     private boolean availability;
-       
-    public LibraryItems(int Id, String Title, String author, double price, String Genre,String type) {
+    public static ArrayList<LibraryItems> libraryItemsList = new ArrayList<>();
+
+    public LibraryItems(int Id, String Title, String Author, double Price, String Genre, String type) {
         this.Id = Id;
         this.Title = Title;
-        this.Author = author;
-        this.Price = price;
+        this.Author = Author;
+        this.Price = Price;
         this.Genre = Genre;
-        this.type=type;
+        this.type = type;
         this.availability = true;
-      
     }
-        
+
     public int getId() {
         return Id;
     }
 
-    public void setid(int Id) {
+    public void setId(int Id) {
         this.Id = Id;
     }
 
@@ -52,16 +53,16 @@ public class LibraryItems {
         return Author;
     }
 
-    public void setAuthor(String author) {
-        this.Author = author;
+    public void setAuthor(String Author) {
+        this.Author = Author;
     }
 
     public double getPrice() {
         return Price;
     }
 
-    public void setPrice(double price) {
-        this.Price = price;
+    public void setPrice(double Price) {
+        this.Price = Price;
     }
 
     public String getGenre() {
@@ -71,80 +72,59 @@ public class LibraryItems {
     public void setGenre(String Genre) {
         this.Genre = Genre;
     }
-    
-     public String getType() {
+
+    public String getType() {
         return type;
     }
 
-    
     public void setType(String type) {
         this.type = type;
     }
-    public void setAvailability(boolean aAvailability) { 
-            availability = aAvailability;
-        }
-    public  boolean isAvailability() { 
+
+    public boolean isAvailability() {
         return availability;
     }
 
- public boolean bookAvailability(String bookName) {
- 
-      boolean found = false;
-        for (LibraryItems item : libraryItemsList) {
-            if (item.getTitle().equalsIgnoreCase(bookName)) {
-                System.out.println("Match for '" + bookName + "' was found.");             
-                item.setAvailability(true); 
-                found = true;
-                return true;
-            }
-        }
-         if(!found){
-             System.out.println("Match for "+"''"+bookName+"''"+" was not find, Book is not available"); 
-             
-         }
-         return false;
- }
- public static LibraryItems findBookById(int bookID) {
+    public void setAvailability(boolean aAvailability) {
+        this.availability = aAvailability;
+    }
+
+    public static LibraryItems findBookById(int bookID) {
         for (LibraryItems book : libraryItemsList) {
             if (book.getId() == bookID) {
-                return book;  // Return the book if found
+                return book;
             }
         }
-        return null;  // Return null if no book is found 
+        return null;
     }
-public static LibraryItems findBookByTitle(String title) {
+
+    public static LibraryItems findBookByTitle(String title) {
         for (LibraryItems book : libraryItemsList) {
             if (book.getTitle().equalsIgnoreCase(title)) {
-                return book; // Return the book 
+                return book;
             }
         }
-        return null; // Return null if not found
+        return null;
     }
 
     @Override
     public String toString() {
-        return  "[ "+type+" Title=" + Title + ",Author=" + Author + " ,Price=" + Price + 
-               " ,Availability=" + (isAvailability() ? "Available" : "Not Available") + ",Genre=" + Genre;}
-
+        return "[ " + type + ", Title= " + Title + ", Author= " + Author + ", Price= " + Price +
+                ", Availability= " + (availability ? "Available" : "Not Available") + ", Genre= " + Genre + " ]";
+    }
 }
 
-
-class Book extends LibraryItems {
-    private int printYear;
+class EBook extends LibraryItems {
     private String language;
+    private String fileFormat;
+    private String downloadLink;
 
-    public Book(int id, String title, String author, double price, String genre, String type, int printYear, String language) {
-        super(id, title, author, price, genre, type);
-        this.printYear = printYear;
+    public EBook(int id, String title, String author, double price, String genre,
+                 String language, String fileFormat, String downloadLink) {
+        super(id, title, author, price, genre, "EBook");
         this.language = language;
-    }
-
-    public int getPrintYear() {
-        return printYear;
-    }
-
-    public void setPrintYear(int printYear) {
-        this.printYear = printYear;
+        this.fileFormat = fileFormat;
+        this.downloadLink = downloadLink;
     }
 
     public String getLanguage() {
@@ -153,30 +133,6 @@ class Book extends LibraryItems {
 
     public void setLanguage(String language) {
         this.language = language;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() +
-                ", print Year: " + printYear +
-                ", Language: " + language+" ]";
-    }
-}
-
-// Subclass for eBooks
-class EBook extends LibraryItems {
-    private String fileFormat;
-    private int publishYear;
-    private String language;
-    private int pages;
-
-    public EBook(int id, String title, String author, double price, String genre, 
-            String type, String fileFormat, int publishYear, String language, int pages) {
-        super(id, title, author, price, genre, type);
-        this.fileFormat = fileFormat;
-        this.publishYear = publishYear;
-        this.language = language;
-        this.pages = pages;
     }
 
     public String getFileFormat() {
@@ -187,68 +143,39 @@ class EBook extends LibraryItems {
         this.fileFormat = fileFormat;
     }
 
-    public int getPublishYear() {
-        return publishYear;
+    public String getDownloadLink() {
+        return downloadLink;
     }
 
-    public void setPublishYear(int publishYear) {
-        this.publishYear = publishYear;
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
     }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
 
     @Override
     public String toString() {
-        return super.toString() +
-                ", File Format: " + fileFormat +
-                ", Publish Year: " + publishYear +
-                ", Language: " + language +
-                ", Pages: " + pages+" ]";
+        return super.toString() + ", Language= " + language +
+                ", Format= " + fileFormat + ", Link= " + downloadLink + " ]";
     }
 }
-class BorrowedBooks extends LibraryItems {
+
+class BorrowedBooks {
     private static ArrayList<LibraryItems> borrowedBooksList = new ArrayList<>();
 
-    BorrowedBooks(int Id, String Title, String Author, double Price, String Genre, String type) {
-        super(Id, Title, Author, Price, Genre, type); 
-    }
-
-    public static void borrowBook() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the title of the book you want to borrow: ");
-        String bookTitle = scanner.nextLine();
-
-        // Check if the book is available in the library
+    public static void borrowBook(String bookTitle) {
         for (LibraryItems book : LibraryItems.libraryItemsList) {
             if (book.getTitle().equalsIgnoreCase(bookTitle)) {
                 if (book.isAvailability()) {
-                    book.setAvailability(false); // Mark as borrowed
-                    borrowedBooksList.add(book); // Add to borrowed list
+                    book.setAvailability(false);
+                    borrowedBooksList.add(book);
                     System.out.println("You have successfully borrowed \"" + book.getTitle() + "\".");
                     return;
                 } else {
-                    System.out.println("Sorry, \"" + book.getTitle() + "\" is already borrowed.");
+                    System.out.println("\"" + book.getTitle() + "\" is already borrowed.");
                     return;
                 }
             }
         }
-
-        System.out.println("Sorry, the book \"" + bookTitle + "\" is not in our library.");
+        System.out.println("The book \"" + bookTitle + "\" is not in the library.");
     }
 
     public static void displayBorrowedBooks() {
@@ -260,6 +187,6 @@ class BorrowedBooks extends LibraryItems {
                 System.out.println("- " + book.getTitle() + " by " + book.getAuthor());
             }
         }
-    }}
-
+    }
+}
 
