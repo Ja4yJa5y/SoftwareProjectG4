5ccee612f61cb12889c6e2bdd5b501c7dcad09b2
GalaZ1 <gallanj11@gmail.com>
2025-12-12 01:52:35 +0300

    Add files via upload

---
+++ MYLibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mylibrary;
+
+/**
+ *
+ * @author k5n5k
+ */
+public class MYLibrary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
